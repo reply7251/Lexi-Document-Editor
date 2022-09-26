6a779b2003b89189c41a394919521223c81ce31c
YapJie <B10923075@gemail.yuntech.edu.tw>
2022-09-26 19:23:01 +0800

    new porn

--- src/me/sa_g6/main/Application.java
+++ src/me/sa_g6/main/Application.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 
 public class Application {
     public static void main(String[] args){
+
         SwingUtilities.invokeLater(()->{
             JFrame mw = new MainWindow();
             mw.setVisible(true);
