a90d7f005cca26b550c5a158ac916eceb06de30f
BuildTools <unconfigured@null.spigotmc.org>
2022-10-15 00:23:49 +0800

    .

--- src/me/sa_g6/ui/widgets/DisplayModeMenuItem.java
+++ src/me/sa_g6/ui/widgets/DisplayModeMenuItem.java
@@ -5,11 +5,9 @@ import me.sa_g6.formatting.Font.Font;
 import me.sa_g6.ui.MainWindow;
 
 public class DisplayModeMenuItem extends IMenuItem {
-        public DisplayModeMenuItem(MainWindow mw, String text, DisplayMode displayMode) {
+        public DisplayModeMenuItem(String text, DisplayMode displayMode) {
             super(text, (e)->{
-                if(mw.getCurrentTab() instanceof Tab tab){
-                    displayMode.perform(tab.getEditor());
-                }
+                    displayMode.perform(MainWindow.getInstance().getCurrentTab().getEditor());
             });
         }
 
