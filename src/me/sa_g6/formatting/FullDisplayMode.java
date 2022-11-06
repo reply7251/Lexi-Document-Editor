dff1701752a9724e49d62bcb0cc6a080a373b78a
Rikurzt <rikurzt@gmail.com>
2022-11-06 19:05:27 +0800

    add display mode

--- src/me/sa_g6/formatting/FullDisplayMode.java
+++ src/me/sa_g6/formatting/FullDisplayMode.java
@@ -1,18 +1,23 @@
 package me.sa_g6.formatting;
 
+import me.sa_g6.ui.MainWindow;
+
 import javax.swing.*;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 
 public class FullDisplayMode implements DisplayMode{
     JTextPane curJTextPane ;
+
+
+
     @Override
     public void perform(JTextPane editor) {
         curJTextPane=editor;
         SimpleAttributeSet attr = new SimpleAttributeSet();
         StyleConstants.setSpaceAbove(attr,8);
         StyleConstants.setSpaceBelow(attr,8);
-
+        MainWindow.getInstance().getCurrentTab().puretext=false;
         editor.getStyledDocument().setParagraphAttributes(0,Integer.MAX_VALUE,attr,false);
     }
     public void perform() {
--- src/me/sa_g6/formatting/TextOnlyDisplayMode.java
+++ src/me/sa_g6/formatting/TextOnlyDisplayMode.java
@@ -1,27 +1,31 @@
 package me.sa_g6.formatting;
 
 import me.sa_g6.formatting.DisplayMode;
+import me.sa_g6.ui.MainWindow;
+import me.sa_g6.ui.widgets.EnhancedHTMLDocument;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
+import java.awt.event.KeyEvent;
 
 public class TextOnlyDisplayMode implements DisplayMode {
     JTextPane curJTextPane;
+    MainWindow mainWindow;
+
+
+
     @Override
 
     public void perform(JTextPane editor) {
+        System.out.println("eee");
         curJTextPane=editor;
         SimpleAttributeSet attr = new SimpleAttributeSet();
         StyleConstants.setSpaceAbove(attr,1);
         StyleConstants.setSpaceBelow(attr,1);
-        int docunmentLength = editor.getComponentCount();
         editor.getStyledDocument().setParagraphAttributes(0,Integer.MAX_VALUE,attr,false);
+
     }
-    public void perform() {
-        SimpleAttributeSet attr = new SimpleAttributeSet();
-        StyleConstants.setSpaceAbove(attr,8);
-        StyleConstants.setSpaceBelow(attr,8);
-        curJTextPane.getStyledDocument().setParagraphAttributes(0,Integer.MAX_VALUE,attr,false);
-    }
+
 }
